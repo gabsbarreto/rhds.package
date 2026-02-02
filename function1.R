095eb61ffb8b7afef5b8739c9f1ee614930fd015
gabsbarreto <gabriel.castanhob@usp.br>
2026-02-02 14:55:43 +0000

    removed cached function1

--- function1.R
+++
@@ -1,9 +0,0 @@
-#' Perform a simple arithmetic mean of two values.
-#' Parameters x and y are the values you want the mean of
-#' @return mean value
-#' @export
-
-mean_two <- function(x,y) {
-newmean = (x + y) / 2
-return(newmean)
-}
